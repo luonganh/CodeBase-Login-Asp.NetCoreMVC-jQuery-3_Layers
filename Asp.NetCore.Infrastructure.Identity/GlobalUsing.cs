dc37ce41736123eb23a96d61fedd0dd045d0b773
LuongAnh <luonganh@gmail.com>
2024-02-27 11:15:36 +0700

    Add dbcontext

--- Asp.NetCore.Infrastructure.Identity/GlobalUsing.cs
+++ Asp.NetCore.Infrastructure.Identity/GlobalUsing.cs
@@ -1,3 +1,6 @@
 ﻿global using System.ComponentModel.DataAnnotations;
 global using Asp.NetCore.Infrastructure.Identity.Enums;
 global using Microsoft.AspNetCore.Identity;
+global using Asp.NetCore.Infrastructure.Identity.Entities;
+global using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore;
---
+++ Asp.NetCore.Infrastructure.Identity/IdentityContext.cs
@@ -0,0 +1,22 @@
+﻿namespace Asp.NetCore.Infrastructure.Identity
+{
+    public class IdentityContext : IdentityDbContext<AppUser, AppRole, Guid>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<AppRole> AppRoles { set; get; }
+        public DbSet<AppUser> AppUsers { set; get; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims").HasKey(x => x.Id);
+            builder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims").HasKey(x => x.Id);
+            builder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(x => x.UserId);
+            builder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x => new { x.RoleId, x.UserId });
+            builder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(x => new { x.UserId });            
+        }
+    }
+}
